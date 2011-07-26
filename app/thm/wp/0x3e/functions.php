d90d0536b04dd8fa886b5d2ae061ec149d1130d1
0x3e <0x3e.com@gmail.com>
2011-07-26 23:03:35 +1000

    .

--- app/thm/wp/0x3e/functions.php
+++ app/thm/wp/0x3e/functions.php
@@ -3,3 +3,4 @@ Dev::log("info","theme functions.php");
 remove_action('wp_head', 'wlwmanifest_link');
 remove_action('wp_head', 'rsd_link');
 remove_action('wp_head', 'wp_generator');
+remove_filter ('the_content', 'wpautop');
